226458ab0b8b5bd540aae6ceaee47b7cab78e49b
chenmtech <chenm91@163.com>
2020-10-06 06:44:43 +0800

    Update ReturnCode.java

--- src/com/cmtech/web/btdevice/ReturnCode.java
+++ src/com/cmtech/web/btdevice/ReturnCode.java
@@ -2,7 +2,7 @@ package com.cmtech.web.btdevice;
 
 public enum ReturnCode {
 	SUCCESS(0),
-	WEB_ERR(1),
+	WEB_ERR(1), // never used in the web node
 	INVALID_PARA_ERR(2),
 	SIGNUP_ERR(3),
 	LOGIN_ERR(4),
