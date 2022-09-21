4c98419a7573f9b7dfd53797411ffef33af6d546
chenmtech <1276094137@qq.com>
2022-09-21 09:12:59 +0800

    Update UploadDownloadFileServlet.java

--- src/com/cmtech/web/servlet/UploadDownloadFileServlet.java
+++ src/com/cmtech/web/servlet/UploadDownloadFileServlet.java
@@ -24,8 +24,8 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 @WebServlet(name="UploadDownloadFileServlet", urlPatterns="/File")
 public class UploadDownloadFileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private ServletFileUpload uploader = null;
-    private File path = new File(System.getProperty("catalina.home")+File.separator + "ecg");
+	private ServletFileUpload uploader = null;
+	private File path = new File(System.getProperty("catalina.home")+File.separator + "DATA");
     
 	@Override
 	public void init() throws ServletException{
@@ -36,22 +36,24 @@ public class UploadDownloadFileServlet extends HttpServlet {
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String fileName = request.getParameter("fileName");
-		if(fileName == null || fileName.equals("")){
-			throw new ServletException("File Name can't be null or empty");
-		}
-		File file = new File(path, fileName);
-		if(!file.exists()){
-			throw new ServletException("File doesn't exists on server.");
-		}
-		System.out.println("File location on server::"+file.getAbsolutePath());
+		 String fileName = request.getParameter("fileName"); 
+		 if(fileName == null || fileName.equals("")){ 
+			 throw new ServletException("File Name can't be null or empty"); 
+		 } 
+		 File typePath = new File(path, "ECG"); 
+		 File file = new File(typePath, fileName);
+		 if(!file.exists()){ 
+			  throw new ServletException("File doesn't exists on server:" + file.getAbsolutePath());
+		 }
+		 System.out.println("File location on server::"+file.getAbsolutePath());
+		 
 		ServletContext ctx = getServletContext();
 		InputStream fis = new FileInputStream(file);
 		String mimeType = ctx.getMimeType(file.getAbsolutePath());
 		response.setContentType(mimeType != null? mimeType:"application/octet-stream");
 		response.setContentLength((int) file.length());
 		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-		
+				
 		ServletOutputStream os       = response.getOutputStream();
 		byte[] bufferData = new byte[1024];
 		int read=0;
@@ -82,17 +84,20 @@ public class UploadDownloadFileServlet extends HttpServlet {
 				System.out.println("ContentType="+fileItem.getContentType());
 				System.out.println("Size in bytes="+fileItem.getSize());
 				
-				File file = new File(path, fileItem.getName());
+				File typePath = new File(path, fileItem.getFieldName());
+				if(!typePath.exists()) typePath.mkdirs();
+				File file = new File(typePath, fileItem.getName());
+				if(file.exists()) file.delete();
 				System.out.println("Absolute Path at server="+file.getAbsolutePath());
 				fileItem.write(file);
 				out.write("File "+fileItem.getName()+ " uploaded successfully.");
 				out.write("<br>");
-				out.write("<a href=\"File?fileName="+fileItem.getName()+"\">Download "+fileItem.getName()+"</a>");
+				out.write("<a href=\"File?fileName="+fileItem.getName()+"\">Download "+file.getAbsolutePath()+"</a>");
 			}
 		} catch (FileUploadException e) {
 			out.write("FileUploadException in uploading file.");
 		} catch (Exception e) {
-			out.write("Exception in uploading file.");
+			out.write("Exception in uploading file." + e);
 		}
 		out.write("</body></html>");
 	}
