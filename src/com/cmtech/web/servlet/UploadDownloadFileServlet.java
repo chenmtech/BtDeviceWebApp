484ff1cfbdaeddf46aa4215c3e258cf849c79f5c
chenmtech <1276094137@qq.com>
2022-09-21 11:17:33 +0800

    Update UploadDownloadFileServlet.java

--- src/com/cmtech/web/servlet/UploadDownloadFileServlet.java
+++ src/com/cmtech/web/servlet/UploadDownloadFileServlet.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,35 +16,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+/**
+ * 实现信号数据文件的上传和下载
+ * @author chenm
+ *
+ */
 @WebServlet(name="UploadDownloadFileServlet", urlPatterns="/File")
 public class UploadDownloadFileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ServletFileUpload uploader = null;
-	private File path = new File(System.getProperty("catalina.home")+File.separator + "DATA");
+	private File sigPath = new File(System.getProperty("catalina.home")+File.separator + "DATA");
     
 	@Override
 	public void init() throws ServletException{
-    	if(!path.exists()) path.mkdirs();
+    	if(!sigPath.exists()) sigPath.mkdirs();
 		DiskFileItemFactory fileFactory = new DiskFileItemFactory();
-		fileFactory.setRepository(path);
+		fileFactory.setRepository(sigPath);
 		this.uploader = new ServletFileUpload(fileFactory);
 	}
 	
+	/**
+	 * 用于实现文件下载服务
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 String fileName = request.getParameter("fileName"); 
+		String sigType = request.getParameter("sigType"); 
+		String fileName = request.getParameter("fileName"); 
 		 if(fileName == null || fileName.equals("")){ 
 			 throw new ServletException("File Name can't be null or empty"); 
 		 } 
-		 File typePath = new File(path, "ECG"); 
+		 
+		 File typePath = new File(sigPath, sigType); 
 		 File file = new File(typePath, fileName);
 		 if(!file.exists()){ 
 			  throw new ServletException("File doesn't exists on server:" + file.getAbsolutePath());
 		 }
-		 System.out.println("File location on server::"+file.getAbsolutePath());
+		 //System.out.println("File location on server::"+file.getAbsolutePath());
 		 
 		ServletContext ctx = getServletContext();
 		InputStream fis = new FileInputStream(file);
@@ -63,43 +71,42 @@ public class UploadDownloadFileServlet extends HttpServlet {
 		os.flush();
 		os.close();
 		fis.close();
-		System.out.println("File downloaded at client successfully");
+		//System.out.println("File downloaded at client successfully");
 	}
 
+	/**
+	 * 用于实现文件上传服务
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if(!ServletFileUpload.isMultipartContent(request)){
 			throw new ServletException("Content type is not multipart/form-data");
 		}
 		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.write("<html><head></head><body>");
+		File file = null;
 		try {
 			List<FileItem> fileItemsList = uploader.parseRequest(request);
 			Iterator<FileItem> fileItemsIterator = fileItemsList.iterator();
 			while(fileItemsIterator.hasNext()){
 				FileItem fileItem = fileItemsIterator.next();
-				System.out.println("FieldName="+fileItem.getFieldName());
-				System.out.println("FileName="+fileItem.getName());
-				System.out.println("ContentType="+fileItem.getContentType());
-				System.out.println("Size in bytes="+fileItem.getSize());
+				//System.out.println("FieldName="+fileItem.getFieldName());
+				//System.out.println("FileName="+fileItem.getName());
+				//System.out.println("ContentType="+fileItem.getContentType());
+				//System.out.println("Size in bytes="+fileItem.getSize());
 				
-				File typePath = new File(path, fileItem.getFieldName());
+				File typePath = new File(sigPath, fileItem.getFieldName());
 				if(!typePath.exists()) typePath.mkdirs();
-				File file = new File(typePath, fileItem.getName());
-				if(file.exists()) file.delete();
-				System.out.println("Absolute Path at server="+file.getAbsolutePath());
-				fileItem.write(file);
-				out.write("File "+fileItem.getName()+ " uploaded successfully.");
-				out.write("<br>");
-				out.write("<a href=\"File?fileName="+fileItem.getName()+"\">Download "+file.getAbsolutePath()+"</a>");
+				
+				file = new File(typePath, fileItem.getName());
+				//System.out.println("Absolute Path at server="+file.getAbsolutePath());
+				
+				if(!file.exists()) {
+					fileItem.write(file);
+				}
 			}
-		} catch (FileUploadException e) {
-			out.write("FileUploadException in uploading file.");
 		} catch (Exception e) {
-			out.write("Exception in uploading file." + e);
+			if(file != null && file.exists())
+				file.delete();
 		}
-		out.write("</body></html>");
 	}
 
 }
